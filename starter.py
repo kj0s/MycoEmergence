294166c8c374630e09ff096fc6904edcb606547c
KJ <ksheerjas@gmail.com>
2025-11-14 10:49:25 +1100

    Update starter.py
    code has a starting 'energy' value. this does not look at each path in its entirety, finds the locally optimum path, and adds that value to its total nutrient/energy holder. then it deducts a fixed energy cost, and finds the best path to move.

--- starter.py
+++ starter.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
+ENERGY_START = 5
+
 def make_board(n):
     nutrients = np.random.randint(0, 10, size=(n, n))
     plt.imshow(nutrients, cmap='YlGn')
@@ -15,61 +17,46 @@ def get_neighbours(board, x, y):
               if 0 <= x + dx < len(board) and 0 <= y + dy < len(board[0])]
     return ((board[nx][ny], nx, ny) for nx, ny in coords)
 
+
+
 def move(board):
-    rows = len(board)
-    cols = len(board[0])
+    rows = random.randint(0, len(board)-1)
+    cols = random.randint(0, len(board[0])-1)
+    start = board[rows][cols]
+    stack = [(start, rows, cols)]
+    path = [(rows, cols)]
+    energy = ENERGY_START
+    while stack and energy > 0:
+        ogval, ox, oy = stack.pop()
+        possible = []
+        for newval, nx, ny in get_neighbours(board, ox, oy):
+            if (nx, ny) not in path:
+                possible.append((newval, nx, ny))
+        if not possible:
+            break
+        best = sorted(possible, key=lambda x: x[0], reverse=True)[0]
+        path.append((best[1], best[2]))
+        energy += best[0] - 1
+        stack.append((best[0], best[1], best[2]))
+    return path
 
-    memo = {}  # (x,y)
-
-    def dfs(x, y):
-        if (x, y) in memo:
-            return memo[(x, y)]
-
-        start_val = board[x][y]
-        best_sum = start_val
-        best_path = [(x, y)]
-
-        for newval, nx, ny in get_neighbours(board, x, y):
-            if newval > start_val:
-                sub_sum, sub_path = dfs(nx, ny)
-                new_total = start_val + sub_sum
-
-                if new_total > best_sum:
-                    best_sum = new_total
-                    best_path = [(x, y)] + sub_path
-
-        memo[(x, y)] = (best_sum, best_path)
-        return memo[(x, y)]
-
-    global_best_sum = 0
-    global_best_path = []
-
-    for row in range(rows):
-        for col in range(cols):
-            path_sum, path = dfs(row, col)
-            if path_sum > global_best_sum:
-                global_best_sum = path_sum
-                global_best_path = path
-
-    return global_best_sum, global_best_path
 
 def draw_path(board, path):
     plt.figure(figsize=(6,6))
     plt.imshow(board, cmap='YlGn')
     plt.colorbar(label="Nutrient level")
 
-    xs = [y for x, y in path]   
+    xs = [y for x, y in path]
     ys = [x for x, y in path]
 
     plt.plot(xs, ys, color="red", linewidth=2, marker="o")
     plt.title("Optimal Path")
-    plt.gca().invert_yaxis()  
+    plt.gca().invert_yaxis()
     plt.show()
 
 board = make_board(10)
-best_sum, best_path = move(board)
+path = move(board)
 
-print("Best score:", best_sum)
-print("Path:", best_path)
+print("Path:", path)
 
-draw_path(board, best_path)
+draw_path(board, path)
